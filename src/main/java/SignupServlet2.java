f05da77b952b0860458932c39b097ada44b84558
Furebo <furebodidace582@gmail.com>
2022-12-13 07:33:32 +0200

    fixed the error in login

--- src/main/java/SignupServlet2.java
+++ src/main/java/SignupServlet2.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SignupServlet2
@@ -23,7 +24,8 @@ public class SignupServlet2 extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.addHeader("Access-Control-Allow-Origin","*");
+		        res.addHeader("Access-Control-Allow-Origin","*");
+		        
 	            String IncomingFirstName=req.getParameter("firstname");
 	            String IncomingLastName=req.getParameter("lastname");
 	            String IncomingEmail=req.getParameter("email");
@@ -47,7 +49,7 @@ public class SignupServlet2 extends HttpServlet {
     		
     	     void signup(String UserRole){
     	    		Users user = new Users(firstname,lastname,email,password,UserRole);
-    	    		System.out.println(user);
+    	    		//System.out.println(user);
     	    		HashMap<String,String>newusermap = new HashMap<String,String>();
     	    		newusermap.put("firstname", user.firstname);
     	    		newusermap.put("lastname", user.lastname);
@@ -63,10 +65,23 @@ public class SignupServlet2 extends HttpServlet {
     	    		
     	    };
     	};
-    	if(IncomingPassword.length()==4) {
+    	if(IncomingPassword.length()==4||IncomingPassword.length()==6||IncomingPassword.length()==8||IncomingPassword.length()==10) {
     		Users Patient = new Users(IncomingFirstName,IncomingLastName,IncomingEmail,IncomingPassword,"Patient");
-    	    Patient.signup("Patient");
-    	    System.out.println("The role of the user is: " + Patient.role);
+    	    if(IncomingPassword.length()==4) {
+    	    	Patient.signup("Patient");
+    	    }else if(IncomingPassword.length()==6) {
+    	    	Patient.signup("Phyarmacist");
+    	    }else if(IncomingPassword.length()==8) {
+    	    	Patient.signup("Physician");
+    	    }else if(IncomingPassword.length()==10) {
+    	    	Patient.signup("Admin");
+    	    }
+    		
+    	    //System.out.println("The role of the user is: " + Patient.role);
+    	    
+    	    HttpSession session = req.getSession();
+        	session.setAttribute("userdata", usermap);
+        	
     		res.sendRedirect("http://localhost:3000/login");
     	}
     	
--- src/main/java/UserLogin.java
+++ src/main/java/UserLogin.java
@@ -7,75 +7,82 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class UserLogin extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.addHeader("Access-Control-Allow-Origin","*");
-    	String password = req.getParameter("password");
-    	String email = req.getParameter("email");
+    	String IncomingPassword = req.getParameter("password");
+    	String IncomingEmail = req.getParameter("email");
+    	
+    
+    	HttpSession session = req.getSession();
+    	HashMap<String,HashMap<String,String>> userdata = (HashMap<String,HashMap<String,String>>)session.getAttribute("userdata");
+    	
+    	System.out.println(userdata);
+    	
+    	HashMap<String,String> IncomingUser=new HashMap<String,String>();
+    	
+    	 for(Entry<String,HashMap<String,String>> m : userdata.entrySet()) {
+      	    IncomingUser=m.getValue();
+      	}
+    	 System.out.println(IncomingUser);
+    	 
+ 
+    	 for(Entry m: IncomingUser.entrySet()) {
+    		 if( m.getKey().equals("password")&m.getValue().equals(IncomingPassword)) {
+    			 
+    				if(IncomingPassword.length()==4) {
+    					SignupServlet2 signedeup = new SignupServlet2();
+    			    	try {
+    						res.sendRedirect("http://localhost:3000/patients");
+    					} catch (IOException e) {
+    						// TODO Auto-generated catch block
+    						e.printStackTrace();
+    					}
+    					
+    				}else if(IncomingPassword.length()==6) {
+    					try {
+    						res.sendRedirect("http://localhost:3000/pharmacists");
+    					} catch (IOException e) {
+    						// TODO Auto-generated catch block
+    						e.printStackTrace();
+    					}
+    				}else if(IncomingPassword.length()==8) {
+    					try {
+    						res.sendRedirect("http://localhost:3000/physicians");
+    					} catch (IOException e) {
+    						// TODO Auto-generated catch block
+    						e.printStackTrace();
+    					}
+    				}else if(IncomingPassword.length()==8) {
+    					try {
+    						res.sendRedirect("http://localhost:3000/users");
+    					} catch (IOException e) {
+    						// TODO Auto-generated catch block
+    						e.printStackTrace();
+    					}
+    				}
+    			 
+    		 }
+    	 }
+    	
+ 	
     	
     	 class Login extends User {
     		String email;
     		String password;
     		
     		Login(String email, String password){
-    			this.email=email;
-    			this.password=password;
+    			this.email=IncomingEmail;
+    			this.password=IncomingPassword;
     		}
-    		void login() {
-    			SignupServlet2 signedeup = new SignupServlet2();
-    			if(password.length()==4) {
-    		    	for(Entry user : signedeup.usermap.entrySet()) {
-    		    		if(user.getKey()=="pharmacist") {
-    		    			try {
-								res.sendRedirect("http://localhost:3000/pharmacists");
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-    		    		}
-    		    	}
-    				
-    			}else if(password.length()==6) {
-    				for(Entry user : signedeup.usermap.entrySet()) {
-    					if(user.getKey()=="patient") {
-    						try {
-								res.sendRedirect("http://localhost:3000/patients");
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-    					}
-    				}
-    			}else if(password.length()==8) {
-    				for(Entry user : signedeup.usermap.entrySet()) {
-    					if(user.getKey()=="physician") {
-    						try {
-								res.sendRedirect("http://localhost:3000/physicians");
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-    					}
-    				}
-    			}else if(password.length()==10) {
-    				for(Entry user : signedeup.usermap.entrySet()) {
-    					if(user.getKey()=="admin") {
-    						try {
-								res.sendRedirect("http://localhost:3000/users");
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-    					}
-    				}
-    			}
-    		};
-    	    void signup() {
-    	    	System.out.println("The user is registered.");
-    	    };
+    		
+    		 void signup() {
+     	    	System.out.println("The user is registered.");
+     	    };
+  
     	}	
-    	
-	}
+    }
 }
